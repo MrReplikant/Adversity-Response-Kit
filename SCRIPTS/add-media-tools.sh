fa186aa1a7dbf3569fe2d697a2f7e08ca4b7e336
MrReplikant <74075031+MrReplikant@users.noreply.github.com>
2023-02-06 08:37:45 -0600

    Update add-media-tools.sh

--- SCRIPTS/add-media-tools.sh
+++ SCRIPTS/add-media-tools.sh
@@ -1,3 +1,21 @@
 #!/bin/bash 
 
+isRoot() {
+        if [ "$EUID" -ne 0 ]; then
+                echo "Must be run as root/with sudo"
+                exit 1
+        fi
+}
+
+
+install_media_packages(){
 apt install kiwix marble vlc
+}
+
+main(){
+isRoot
+install_media_packages
+}
+
+main 
+echo "Media Packages Installed!"
