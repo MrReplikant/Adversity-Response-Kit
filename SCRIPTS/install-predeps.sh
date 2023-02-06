c6641d9be548010c10224c97f784d4bc59b4641d
MrReplikant <74075031+MrReplikant@users.noreply.github.com>
2023-02-06 08:32:15 -0600

    Update install-predeps.sh

--- SCRIPTS/install-predeps.sh
+++ SCRIPTS/install-predeps.sh
@@ -1,3 +1,23 @@
 #!/bin/bash 
 
+
+isRoot() {
+        if [ "$EUID" -ne 0 ]; then
+                echo "Must be run as root/with sudo"
+                exit 1
+        fi
+}
+
+install_predeps(){
 apt install curl dpkg-dev usrmerge dkms build-essential apt-mirror deboostrap
+mkdir /ark
+mkdir /ark/chroots
+}
+
+main(){
+isRoot
+install_predeps
+}
+
+main
+echo "Pre-Dependencies Installed"
